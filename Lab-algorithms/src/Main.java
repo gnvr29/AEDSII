9fdc95dc2e7ad5be62f3f32e51b9e1e519aab9b7
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-03-27 16:00:28 -0300

    feat: tests for question 2 on main

--- Lab-algorithms/src/Main.java
+++ Lab-algorithms/src/Main.java
@@ -1,5 +1,6 @@
 import ExamPractice.Question12;
 import ExamPractice.Question13;
+import ExamPractice.Question2;
 import ExamPractice.Question5;
 import ExamPractice.Question8;
 
@@ -27,5 +28,15 @@ public class Main {
         char[] f = {'d', '4', 'y', '2', 'd'};
         System.out.println(Question13.findPalindromeRecursively(d, 0));
         System.out.println(Question13.findPalindromeRecursively(f, 0));
+        System.out.println("-------------------");
+
+        System.out.println("Question 2 tests:");
+        System.out.println("Expected results: [1, 10] and [4, 12]");
+        int[] g = {7, 5, 2, 4, 6, 10, 1, 9, 3};
+        int[] h = {10, 11, 7, 5, 4, 12, 8, 6};
+        int[] gAnswer = Question2.maxMin(g);
+        int[] hAnswer = Question2.maxMin(h);
+        System.out.println(gAnswer[0] + " " + gAnswer[1]);
+        System.out.println(hAnswer[0] + " " + hAnswer[1]);
     }
 }
